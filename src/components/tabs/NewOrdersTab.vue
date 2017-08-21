8d54e64ac9b085973d0e69c3f544a0ef898efe70
kolya-t <toporkov.kolya96@gmail.com>
2017-08-21 20:19:57 +0300

    Add snackbar instead alert

--- src/components/tabs/NewOrdersTab.vue
+++ src/components/tabs/NewOrdersTab.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <div v-show="loading" style="text-align: center">
-      <v-progress-circular v-show="loading" indeterminate v-bind:size="50" class="primary--text"></v-progress-circular>
+      <v-progress-circular indeterminate v-bind:size="50" class="primary--text"></v-progress-circular>
     </div>
 
-    <div v-show="!loading & !error">
+    <div v-show="orders.length > 0">
       <v-layout row>
         <v-flex xs12 sm6 offset-sm3>
           <v-card>
@@ -15,16 +15,17 @@
                     <v-list-tile-title>{{ order.description }}</v-list-tile-title>
                     <v-list-tile-sub-title class="grey--text text--darken-4">
                       {{ order.price | currency('') }} ₽
-                </v-list-tile-sub-title>
+                    </v-list-tile-sub-title>
                     <v-list-tile-sub-title>{{ order.customerName }}</v-list-tile-sub-title>
                   </v-list-tile-content>
                   <v-list-tile-action>
                     <v-list-tile-action-text>
                       {{ order.createdDate | moment('calendar') }}
-                </v-list-tile-action-text>
-                    <v-icon class="grey--text text--lighten-1" v-tooltip:left="{ html: bages.get(order.status).tooltip }">
+                    </v-list-tile-action-text>
+                    <v-icon class="grey--text text--lighten-1"
+                            v-tooltip:left="{ html: bages.get(order.status).tooltip }">
                       {{ bages.get(order.status).icon }}
-                </v-icon>
+                    </v-icon>
                   </v-list-tile-action>
                 </v-list-tile>
                 <v-divider v-if="index + 1 < orders.length"></v-divider>
@@ -41,11 +42,12 @@
                 <b>{{ selectedOrder.description }}</b><br/>
                 Заказчик: <b>{{ selectedOrder.customerName }}</b><br/>
                 Стоимость: <b>{{ selectedOrder.price | currency('') }} ₽</b>
-          </v-card-text>
+              </v-card-text>
               <v-card-actions>
                 <v-spacer></v-spacer>
                 <v-btn class="green--text darken-1" flat="flat" @click.native="showDialog = false">Отказаться</v-btn>
-                <v-btn class="green--text darken-1" flat="flat" @click.native="showDialog = false; takeOrder()">Взять</v-btn>
+                <v-btn class="green--text darken-1" flat="flat" @click.native="showDialog = false; takeOrder()">Взять
+                </v-btn>
               </v-card-actions>
             </v-card>
           </v-dialog>
@@ -53,13 +55,21 @@
       </v-layout>
     </div>
 
-    <v-alert v-show="error" error value="true">{{ errorText }}</v-alert>
+    <v-snackbar
+      :timeout="5000"
+      :bottom="true"
+      :right="true"
+      v-model="snackbar"
+    >
+      {{ snackbarText }}
+      <v-btn flat class="pink--text" @click.native="snackbar = false">Close</v-btn>
+    </v-snackbar>
   </div>
 </template>
 
 <script>
   import Vue from 'vue'
-  import { getAllOrders, takeOrder } from '../../utils/orders-api'
+  import {getAllOrders, takeOrder} from '../../utils/orders-api'
 
   const moment = require('moment');
   require('moment/locale/ru');
@@ -72,12 +82,11 @@
     name: 'orders',
     data() {
       return {
-        ordersEndpoint: 'http://192.168.1.3:8080/orders/',
         orders: [],
 
         loading: true,
-        error: false,
-        erorText: '',
+        snackbar: false,
+        snackbarText: '',
 
         showDialog: false,
         selectedOrder: '',
@@ -105,11 +114,13 @@
       getAllOrders() {
         getAllOrders().then(orders => {
           this.orders = orders;
+          this.loading = false;
         }).catch(error => {
           console.log(error);
-          this.error = true;
-          this.errorText = 'Ошибка при загрузке списка заказов';
-        })
+          this.loading = false;
+          this.snackbar = true;
+          this.snackbarText = 'Ошибка при загрузке списка заказов';
+        });
       },
       takeOrder(order) {
         takeOrder(order.id);
@@ -125,7 +136,6 @@
     },
     mounted() {
       this.getAllOrders();
-      this.loading = false;
     }
   }
 </script>
